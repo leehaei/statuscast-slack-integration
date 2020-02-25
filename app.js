3c74042b1d7775f5203544d3b077c22a10f5dfa3
Lee, Haeim <Haeim.Lee@ig.ca>
2020-02-25 14:59:17 -0500

    reverted back

--- app.js
+++ app.js
@@ -6,12 +6,16 @@ var session = require('express-session');
 const bodyParser = require('body-parser');
 var incident_name = "";
 const axios = require('axios'); 
+//import { WebClient } from '@slack/web-api';
 require('dotenv').config();
-
+const { createMessageAdapter } = require('@slack/interactive-messages');
 const slackSigningSecret = process.env.SLACK_SIGNING_SECRET;
+const slackInteractions = createMessageAdapter(slackSigningSecret);
 const SLACK_TOKEN = process.env.SLACK_VERIFICATION_TOKEN;
 const SLACK_BOT_TOKEN = process.env.SLACK_BOT_TOKEN;
 
+const web = new WebClient(SLACK_BOT_TOKEN);
+//const web = new WebClient(SLACK_BOT_TOKEN);
 
 var app = express();
 
@@ -20,27 +24,21 @@ app.use(session({
 	resave: true,
 	saveUninitialized: true
 }));
-
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 /*
 const port = process.env.PORT || 3000;
-
 (async () => {
   const server = await slackInteractions.start(port);
   console.log(`Listening for events on ${server.address().port}`);
@@ -56,31 +54,20 @@ app.post('/slack/events', async(request, response) => {
 		default: { response.sendStatus(404); }
 	}
 });
-
 app.post('/create-incident', function(request, response) {
 	var token = request.body.token;
 	if(token === SLACK_TOKEN) {
 		console.log("verified!");
-
 		incident_name = request.body.text;
-		const trigger_id = request.body.trigger_id;
-		
+
+		const { token, trigger_id, user, actions, type } = JSON.parse(request.payload);
+
 		var modal = {
 			"type": "modal",
 			"callback_id": "incident_view",
 			"title": {
 				"type": "plain_text",
 				"text": "Create an Incident",
-				"emoji": true
-			},
-			"submit": {
-				"type": "plain_text",
-				"text": "Submit",
-				"emoji": true
-			},
-			"close": {
-				"type": "plain_text",
-				"text": "Cancel",
 				"emoji": true
 			},
 			"blocks": [
@@ -90,110 +77,18 @@ app.post('/create-incident', function(request, response) {
 						"type": "mrkdwn",
 						"text": "Please fill in the fields to create a StatusCast incident"
 					}
-				},
-				{
-					"type": "divider"
-				},
-				{
-					"type": "input",
-					"element": {
-						"type": "plain_text_input"
-					},
-					"label": {
-						"type": "plain_text",
-						"text": "Incident Title",
-						"emoji": true
-					}
-				},
-				{
-					"type": "input",
-					"element": {
-						"type": "plain_text_input",
-						"multiline": true
-					},
-					"label": {
-						"type": "plain_text",
-						"text": "Incident message",
-						"emoji": true
-					}
-				},
-				{
-					"type": "input",
-					"element": {
-						"type": "checkboxes",
-						"options": [
-							{
-								"text": {
-									"type": "plain_text",
-									"text": "Jira",
-									"emoji": true
-								},
-								"value": "jira"
-							},
-							{
-								"text": {
-									"type": "plain_text",
-									"text": "Jenkins",
-									"emoji": true
-								},
-								"value": "jenkins"
-							},
-							{
-								"text": {
-									"type": "plain_text",
-									"text": "Confluence",
-									"emoji": true
-								},
-								"value": "confluence"
-							},
-							{
-								"text": {
-									"type": "plain_text",
-									"text": "BitBucket",
-									"emoji": true
-								},
-								"value": "bitbucket"
-							},
-							{
-								"text": {
-									"type": "plain_text",
-									"text": "Sonarqube",
-									"emoji": true
-								},
-								"value": "sonarqube"
-							},
-							{
-								"text": {
-									"type": "plain_text",
-									"text": "Whitesource",
-									"emoji": true
-								},
-								"value": "whitesource"
-							},
-							{
-								"text": {
-									"type": "plain_text",
-									"text": "Artifactory",
-									"emoji": true
-								},
-								"value": "artifactory"
-							}
-						]
-					},
-					"label": {
-						"type": "plain_text",
-						"text": "Select all affected components",
-						"emoji": true
-					}
 				}
-			]
+			],
+			"submit": {
+				"type": "plain_text",
+				"text": "Submit"
+			}
 		};
 		const args = {
 			token: SLACK_BOT_TOKEN,
 			trigger_id: trigger_id,
 			view: JSON.stringify(modal)
 		};
-		
 		console.log("Trigger_ID" + trigger_id);
 		const headers = {
 			headers: {
@@ -201,7 +96,7 @@ app.post('/create-incident', function(request, response) {
     			"Authorization": "Bearer " + SLACK_BOT_TOKEN
 			}
 		};
-		
+
 		axios.post('https://slack.com/api/views.open', args)
 		.then(res => {
 			console.log(res);
@@ -213,7 +108,7 @@ app.post('/create-incident', function(request, response) {
 		response.end("Unable to Verify");
 		response.sendStatus(200);
 	}
-	
+
 });
 
 /*
@@ -244,7 +139,6 @@ app.post('/slack/actions', async(request, response) => {
 				text: 'Submit'
 			}
 		};
-
 		const args = {
 			token: SLACK_BOT_TOKEN,
 			trigger_id: trigger_id,
@@ -274,6 +168,4 @@ app.get('/', function(request, response) {
     response.render('login');
 });
 
-
 module.exports = app;
-
