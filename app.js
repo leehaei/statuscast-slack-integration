7797c354b552b20cb29af19f8023e14952b052e5
Lee, Haeim <Haeim.Lee@ig.ca>
2020-02-26 15:55:40 -0500

    updated check

--- app.js
+++ app.js
@@ -219,7 +219,7 @@ app.post('/create-incident', function(request, response) {
 	}
 
 });
-//"{"payload":"{\"type\":\"view_submission\","
+//"{"payload":"{ type\":\"view_submission\","
 
 app.post('/slack/actions', async(request, response) => {
 	var stop = {
@@ -232,7 +232,7 @@ app.post('/slack/actions', async(request, response) => {
 	var section = {
 		"response_action": "errors",
 		"errors": {
-		  "check-block": str
+		  "check-block": body
 		}
 	  };
 
