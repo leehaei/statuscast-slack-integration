a6bd8d70474ed865e73099e90f5cb5786c19c4b8
Lee, Haeim <Haeim.Lee@ig.ca>
2020-02-26 16:10:25 -0500

    update check

--- app.js
+++ app.js
@@ -234,7 +234,7 @@ app.post('/slack/actions', async(request, response) => {
 	var section = {
 		"response_action": "errors",
 		"errors": {
-		  "check-block": body
+		  "check-block": body[0]
 		}
 	  };
 
