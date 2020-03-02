7df6425c30ae48b98db951d952e9eb56ea93d916
Lee, Haeim <Haeim.Lee@ig.ca>
2020-03-02 12:16:04 -0500

    added check for components

--- app.js
+++ app.js
@@ -291,7 +291,7 @@ app.post('/slack/actions', async(request, response) => {
 		"errors": {
 		  "incident_title": title_val,
 		  "incident_message": message_val,
-		  "incident_components": components
+		  "incident_components": JSON.stringify(components)
 		}
 	  };
 	
