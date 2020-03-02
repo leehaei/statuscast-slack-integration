6a7fb2c7485c7f942c74f67dfe763f4ebc6e1a20
Lee, Haeim <Haeim.Lee@ig.ca>
2020-03-02 10:18:20 -0500

    added check for state

--- app.js
+++ app.js
@@ -272,7 +272,7 @@ app.post('/slack/actions', async(request, response) => {
 	var section = {
 		"response_action": "errors",
 		"errors": {
-		  "check-block": request.body.payload.view.state
+		  "check-block": request.body.payload.view
 		}
 	  };
 	
