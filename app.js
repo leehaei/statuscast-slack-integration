907bc75f25e3dc9d1fc82592b88e257a241173ab
Lee, Haeim <Haeim.Lee@ig.ca>
2020-03-02 11:17:52 -0500

    added check for state

--- app.js
+++ app.js
@@ -284,7 +284,7 @@ app.post('/slack/actions', async(request, response) => {
 	var section = {
 		"response_action": "errors",
 		"errors": {
-		  "incident_title": title_val
+		  "incident_title": JSON.stringify(val);
 		}
 	  };
 	
