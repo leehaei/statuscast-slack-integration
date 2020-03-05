f92ad6556d3f7ea6dd4df9e0a39f699545ca8109
Lee, Haeim <Haeim.Lee@ig.ca>
2020-03-05 14:05:42 -0500

    testing token retrival

--- app.js
+++ app.js
@@ -28,8 +28,9 @@ app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 
 
