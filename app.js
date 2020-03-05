4cfee24301be799b5aa15369e1c08924a3967f78
Lee, Haeim <Haeim.Lee@ig.ca>
2020-03-05 13:49:32 -0500

    testing token retrival

--- app.js
+++ app.js
@@ -335,11 +335,7 @@ app.post('/slack/actions', async(request, response) => {
 	  output += "Affected Components: " + components[0] + " " + components[1] + " " + components[2];
 
 	  var output_test;
-	  const data = {
-			grant_type: 'password',
-			username: STATUSCAST_USERNAME,
-			password: STATUSCAST_PASSWORD
-		};
+	  const data = "grant_type=password&username="+STATUSCAST_USERNAME+"&password="+STATUSCAST_PASSWORD;
 
 	const headers = {
 		headers: {
@@ -348,15 +344,7 @@ app.post('/slack/actions', async(request, response) => {
 	};
 
 	if(type == "\"view_submission\"") {
-		axios.post('https://igm-sandbox.statuscast.com/api/v1/token', {
-			username: STATUSCAST_USERNAME, 
-			password: STATUSCAST_PASSWORD, 
-			grant_type: 'password'
-		}, {
-			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-			}
-		})
+		axios.post('https://igm-sandbox.statuscast.com/api/v1/token', data, headers)
 		.then(res => { 
 			output_test = {
 				"response_action": "errors",
