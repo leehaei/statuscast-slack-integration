9d301043ae379316b0c0346b78ca211d96c1f36b
Lee, Haeim <Haeim.Lee@ig.ca>
2020-03-12 13:27:07 -0400

    checking interactive message

--- app.js
+++ app.js
@@ -203,7 +203,7 @@ app.post('/slack/actions', async(request, response) => {
 		});
 	} else if (type === "interactive_message") {
 		//var id = (JSON.stringify(payload.original_message.attachments.fields[0].value)).replace(/['"]+/g, '');
-		response.send(payload.original_message.attachments.fields[0].value);
+		response.send(payload.original_message.attachments);
 	} else {
 		response.sendStatus(200);
 	}
